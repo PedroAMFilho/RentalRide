1f43b1cbb44cefe002898977faa798f5bfa28096
Pedro <pedro.filho.dev@outlook.com>
2024-03-15 02:12:10 -0300

    file rearange to accomodate docker files

---
+++ RentalRide.Infra.Data/DataContext/RentalRideDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Npgsql;
+using Dapper;
+
+namespace RentalRide.Infra.Data.DataContext
+{
+    public class RentalRideDataContext : IDisposable
+    {
+        public NpgsqlConnection Connection { get; set; }
+
+        public RentalRideDataContext() 
+        {
+            Connection = new NpgsqlConnection("Host=rentalride.database;Username=postgres;Password=admin;Database=rentalride");
+            Connection.Open();
+        }
+
+        public void Dispose() 
+        {
+            if (Connection.State != ConnectionState.Closed)
+            Connection.Close();
+        }
+    }
+}
